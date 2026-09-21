7939beef22d3da6166250f042dc7b149829d131f
octta-bs <octta.bs@gmail.com>
2026-09-21 11:41:53 -0600

    Add multiplication table for 3 in TablaDelTres.cs
    Agrego ejercicio de tabla del 3

---
+++ TablaDelTres.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine("3 x " + i + " = " + (3 * i));
+        }
+    }
+}
