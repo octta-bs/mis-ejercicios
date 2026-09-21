1c45212c4ab5a7304fb1ec4e6a37ebf6f8a4973e
octta-bs <octta.bs@gmail.com>
2026-09-21 11:38:33 -0600

    Add ParOImpar.cs to check even or odd numbers
    Agrego ejercicio de par o impar

---
+++ ParOImpar.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int numero = 10;
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("El número es par");
+        }
+        else
+        {
+            Console.WriteLine("El número es impar");
+        }
+    }
+}
